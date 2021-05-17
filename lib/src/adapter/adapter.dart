67310a3426a11a376549b4a4437420757bd8fc1c
Keagan Hilliard <keagan.hilliard@gmail.com>
2021-05-17 14:12:30 -0700

    Made some changes

--- lib/src/adapter/adapter.dart
+++ lib/src/adapter/adapter.dart
@@ -1,9 +1,7 @@
 import 'cast_adapter.dart';
 import 'chromecast_adapter.dart';
-import 'upnp_adapter.dart';
 
 final List<CastAdapter> adapters = [
-  UPnPAdapter(), //0
   ChromeCastAdapter(), //1
   // AirplayMobileAdapter(), //2
 ];
